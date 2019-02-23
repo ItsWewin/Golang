438f0d56a18d1f7c40c552da0b4655d75583ce7f
魏巍 <wewin11235@163.com>
2019-02-23 18:54:46 +0800

    test(learn-go): test go

--- test5.go
+++ test5.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	name1 := "we"
 	name2 := "win"
 	name := name1 + name2
-	// str1 := "我是wewin"
-	// str2 := "wewin"
-	phrase := "val og tt"
+	fmt.Printf("name1: %s, name2: %s, name: %s\n", name1, name2, name)
+
+	phrase := "val og tt 魏巍"
 	fmt.Printf("String: \"%s\"\n", phrase);
 	for index, char := range phrase {
-		fmt.Printf("%-2d    %U    '%c'    %X\n",
-				index, char, char, []byte(string(char)))
+		fmt.Printf("%-2d    %U  %v  '%c'    %X\n",
+				index, char, char, char, []byte(string(char)))
 	}
 
-	fmt.Printf("name1: %s, name2: %s, name: %s\n", name1, name2, name)
-	// fmt.Printf("string: %s\n", string(U+00C5))
+	line := "xxx ddd ttt"
+	i := strings.Index(line, " ")
+	firstWord := line[0:]
+	fmt.Printf("first word: %s\n", firstWord)
+	fmt.Printf("first blank index: %d\n", i)
 }
