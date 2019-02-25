b6a23ffcef1d31cb944b0ce82563a2d92633c861
魏巍 <wewin11235@163.com>
2019-02-25 23:42:02 +0800

    feat(learn-go): download example source code

---
+++ guess_separator.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s file\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+}
