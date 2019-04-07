182df3e3abeb4a6881177717cdaf7bbc419dc159
魏巍 <wewin11235@163.com>
2019-04-07 16:55:45 +0800

    test(learn-go): fix some error

--- test/convertToBin.go
+++ test/convertToBin.go
@@ -7,6 +7,9 @@ import (
 
 func convertToBin(n int) string {
 	result := ""
+	if n == 0 {
+		return "0"
+	}
 	for ; n > 0; n /= 2 {
 		lsb := n % 2
 		result = strconv.Itoa(lsb) + result
@@ -19,5 +22,6 @@ func main() {
 		convertToBin(5),
 		convertToBin(13),
 		convertToBin(123312121),
+		convertToBin(0),
 	)
 }
