6d4de3ba31908409892d603187907c5b0e1c11e3
魏巍 <wewin11235@163.com>
2019-04-15 08:42:09 +0800

    feat(learn-go): demo for learn go

--- channel/channel.go
+++ channel/channel.go
@@ -11,7 +11,7 @@ func worker(id int, c chan int) {
 	}
 }
 
-func createWorker(id int) chan int {
+func createWorker(id int) chan<- int {
 	c := make(chan int)
 	go func() {
 		for {
@@ -23,7 +23,7 @@ func createWorker(id int) chan int {
 }
 
 func channelDemo() {
-	var channels [10]chan int
+	var channels [10]chan<- int
 	for i := 0; i < 10; i++ {
 		channels[i] = createWorker(i)
 	}
