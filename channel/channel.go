ad3ef629600e4a2ab3d034c033610e0019c8cebd
魏巍 <wewin11235@163.com>
2019-04-15 08:38:31 +0800

    test(learn-go): demo for learn go

--- channel/channel.go
+++ channel/channel.go
@@ -11,11 +11,21 @@ func worker(id int, c chan int) {
 	}
 }
 
+func createWorker(id int) chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Printf("Worker %d received %c\n",
+				id, <-c)
+		}
+	}()
+	return c
+}
+
 func channelDemo() {
 	var channels [10]chan int
 	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go worker(i, channels[i])
+		channels[i] = createWorker(i)
 	}
 
 	for i := 0; i < 10; i++ {
